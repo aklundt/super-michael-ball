fb23d546aefaf62f2ce10d5f6a596144be43a4f0
andrewklundtFT <128536693+andrewklundtFT@users.noreply.github.com>
2023-03-31 15:27:40 -0500

    dialogue improved and can read file of dialogue lines

--- super-michael-ball/Assets/scripts/dialogue/dialogue.cs
+++ super-michael-ball/Assets/scripts/dialogue/dialogue.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
+using System.IO;
 using UnityEngine;
 
 public class dialogue : MonoBehaviour
@@ -7,7 +9,7 @@ public class dialogue : MonoBehaviour
     public GameObject player;
     public GameObject cameraHolder;
     public GameObject gameManager;
-    public int dialogueStartLine;
+    public int dialogueCurrentLine;
     public int dialogueEndLine;
 
     // Start is called before the first frame update
@@ -21,6 +23,24 @@ public class dialogue : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Return) && (player.transform.position - transform.position).magnitude <= 6) {
             gameManager.GetComponent<gameManager>().movementEnabled = false;
+            player.GetComponent<Rigidbody>().drag = 300;
+            returnLine();
         }
+    }
+
+    private string returnLine()
+    {
+        using (FileStream fs = File.OpenRead(@"\Users\ak1036178\Desktop\GitTest\super-michael-ball\super-michael-ball\Assets\scripts\dialogue\dialogueLines.txt"))
+        {
+            byte[] b = new byte[1024];
+            UTF8Encoding temp = new UTF8Encoding(true);
+            int readLen;
+            while ((readLen = fs.Read(b, 0, b.Length)) > 0)
+            {
+                Debug.Log(temp.GetString(b, 0, readLen));
+                return temp.GetString(b, 0, readLen);
+            }
+        }
+        return null;
     }
 }
